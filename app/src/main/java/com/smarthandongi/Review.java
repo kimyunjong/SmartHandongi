ced6ca50626f7eb3c286c3b9b1613c04d3838c90
jungdaeun <jungde94@naver.com>
2015-02-22 12:05:46 +0900

    댓글푸시

--- app/src/main/java/com/smarthandongi/Review.java
+++ app/src/main/java/com/smarthandongi/Review.java
@@ -56,9 +56,8 @@ public class Review extends Activity implements View.OnClickListener, AbsListVie
     Button dialog_delete_confirm, dialog_delete_cancel;
     LinearLayout dialog_delete_background;
 
-    PhpUploadReview phpUploadReview;
     PhpDownloadReview phpDownloadReview;
-    PhpPushReview phpPushReview;
+    PhpUploadPushReview phpUploadPushReview;
     Typeface typeface;
 
     int posting_id,position;
@@ -115,7 +114,7 @@ public class Review extends Activity implements View.OnClickListener, AbsListVie
 
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
-        //reg_btn.setBackgroundResource( getItem(position).getLike().compareTo("0") ==0 ? R.drawable.like : R.drawable.not_like);
+
         reg_btn.setOnClickListener(this);
         //del_btn.setOnClickListener(this);
         back_btn.setOnClickListener(this);
@@ -153,16 +152,6 @@ public class Review extends Activity implements View.OnClickListener, AbsListVie
             }
         };
         review_write.addTextChangedListener(watcher);
-        //   search_cancel_btn=(Button)findViewById(R.id.search_cancel_btn);
-        //   search_cancel_btn.setVisibility(View.VISIBLE);
-        //   search_cancel_btn.setOnClickListener(new Button.OnClickListener () {
-        //       public void onClick(View v) {
-        //           group_search.setText("");
-        //           search_cancel_img.setVisibility(View.INVISIBLE);
-        //           search_glass_img.setVisibility(View.VISIBLE);
-        //       }
-        //   })
-
 
     }
     @Override
@@ -242,7 +231,6 @@ public class Review extends Activity implements View.OnClickListener, AbsListVie
                 break;
             }
             case R.id.notify_btn: {
-               // Toast.makeText(this, "신고버튼",Toast.LENGTH_SHORT).show();
                 dialog_delete_reply = new Dialog(context);
                 dialog_delete_reply.requestWindowFeature(Window.FEATURE_NO_TITLE);
                 dialog_delete_reply.setContentView(R.layout.dialog_delete);
@@ -315,17 +303,14 @@ public class Review extends Activity implements View.OnClickListener, AbsListVie
          }
          else
          {
-             Log.d("con",carrier.getContent());
-             phpUploadReview = new PhpUploadReview();
-             phpUploadReview.execute("http://hungry.portfolio1000.com/smarthandongi/review_upload.php?"
+             Log.d("content",carrier.getContent());
+             phpUploadPushReview = new PhpUploadPushReview();
+             phpUploadPushReview.execute("http://hungry.portfolio1000.com/smarthandongi/review_up_push.php?"
                      + "kakao_id=" + carrier.getId()
                      + "&reply_date=" + reply_date
                      + "&kakao_nick=" + carrier.getNickname()
                      + "&posting_id=" + posting_id
                      + "&content=" + carrier.getContent());
-
-             phpPushReview = new PhpPushReview();
-             phpPushReview.execute("http://hungry.portfolio1000.com/smarthandongi/review_push.php" );
 
              carrier.setNickname(reset_kakao_nick);
 
@@ -351,56 +336,6 @@ public class Review extends Activity implements View.OnClickListener, AbsListVie
     public void onScrollStateChanged(AbsListView view, int scrollState) {    }
     @Override
     public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {    }
-
-    private class PhpUploadReview extends AsyncTask<String, Integer, String> {
-        //int posting_id;
-        public PhpUploadReview(){ super();}
-        @Override
-        protected String doInBackground(String... urls) {
-            StringBuilder jsonHtml = new StringBuilder();
-            String review_id = null;
-            try {
-                //연결 URL설정
-                URL url = new URL(urls[0]);
-                //커넥션 객체 생성
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                //연결되었으면
-                if (conn != null) {
-                    conn.setConnectTimeout(10000);
-                    conn.setUseCaches(false);
-                    if(conn.getResponseCode() == HttpURLConnection.HTTP_OK){
-                        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-                        for(;;){
-                            String line = br.readLine();
-
-                            if(line == null) break;
-                            jsonHtml.append(line + "\n");
-                        }
-                        br.close();
-                    }
-                    conn.disconnect();
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-            try {
-                JSONObject root = new JSONObject(jsonHtml.toString());
-                JSONArray ja = root.getJSONArray("results");
-                JSONObject jo = ja.getJSONObject(0);
-                review_id = jo.getString("review_id");
-                Log.d("review_id",review_id);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            return review_id;
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-
-        }
-    }
 
  //=================================================================================================
     public void php_downloadCreate() {
@@ -460,6 +395,7 @@ public class Review extends Activity implements View.OnClickListener, AbsListVie
                     JSONObject jo = ja.getJSONObject(i);
                     review_list.add(new ReviewDatabase(jo.getInt("posting_id"),
                              jo.getInt("review_id"),jo.getString("kakao_id"), jo.getString("kakao_nick"),
+
                              jo.getString("reply_date"), jo.getString("content")
                     ));
                 }
@@ -479,9 +415,9 @@ public class Review extends Activity implements View.OnClickListener, AbsListVie
         }
     }
     //================================================================
-    public class PhpPushReview extends AsyncTask<String, android.R.integer, String> {
+    public class PhpUploadPushReview extends AsyncTask<String, android.R.integer, String> {
 
-        public PhpPushReview() {
+        public PhpUploadPushReview() {
             super();
         }
 
